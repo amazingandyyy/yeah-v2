71f6390b599fc7f7f7f2e8ddfc6765d29531fae4
Amazingandyyy <amazingandyyy@gmail.com>
2017-01-15 03:14:42 -0800

    add default jwt secret

--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-    jwt_secret: process.env.JWT_SECRET,
+    jwt_secret: process.env.JWT_SECRET || 'JWT_SECRETjwt_secretJWT_SECRET',
     mongo_uri: process.env.MONGODB_URI
 }
 
