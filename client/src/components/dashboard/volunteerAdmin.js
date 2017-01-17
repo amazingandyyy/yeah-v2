b226c6e699ba6e92a5f65d6cdd08ed12b477ce39
Dios <David@Daweis-MacBook-Pro.local>
2017-01-17 13:51:12 -0800

    Create VolunteerAdmin Page

--- client/src/components/dashboard/volunteerAdmin.js
+++ client/src/components/dashboard/volunteerAdmin.js
@@ -14,62 +14,63 @@ class VolunteerAdmin extends Component{
             <form
                 onSubmit={this.props.handleSubmit(this.handleFormSubmit.bind(this))}
             >
-            <fieldset className="form-group">
-              <h3>Volunteer Event Details:</h3>
-              <div>
-              <lable>Activity Name: </lable>
-              <Field 
-                type="text" 
-                name="title" 
-                component="input" 
-                className="form-control"
-                required />
-              </div>
-              <div>
-               <lable>Organization: </lable>
-              <Field 
-                type="text" 
-                name="oranization" 
-                component="input" 
-                className="form-control"
-                required
-              />
-              </div>
-              <div>
-               <lable>When: </lable>
-              <Field 
-                type="text" 
-                name="time" 
-                component="input" 
-                className="form-control"
-                required
-              />
-              </div>
+                <fieldset className="form-group">
+                <h3>Volunteer Event Details:</h3>
+                <div>
+                    <lable>Activity Name: </lable>
+                    <Field 
+                        type="text" 
+                        name="title" 
+                        component="input" 
+                        className="form-control"
+                        required 
+                    />
+                </div>
+                <div>
+                    <lable>Organization: </lable>
+                    <Field 
+                        type="text" 
+                        name="oranization" 
+                        component="input" 
+                        className="form-control"
+                        required
+                    />
+                </div>
+                <div>
+                    <lable>When: </lable>
+                    <Field 
+                        type="text" 
+                        name="time" 
+                        component="input" 
+                        className="form-control"
+                        required
+                    />
+                </div>
 
-              <div>
-               <lable>Where: </lable>
-              <Field 
-                type="text" 
-                name="location" 
-                component="input" 
-                className="form-control"
-                required
-              />
-              </div>
+                <div>
+                    <lable>Where: </lable>
+                    <Field 
+                        type="text" 
+                        name="location" 
+                        component="input" 
+                        className="form-control"
+                        required
+                    />
+                </div>
 
-              <div>
-               <lable>Desctiption: </lable>
-              <Field 
-                type="text" 
-                name="desctiption" 
-                component="textarea" 
-                className="form-control"
-                required
-              />
-              </div>
+                <div>
+                    <lable>Desctiption: </lable>
+                    <Field 
+                        type="text" 
+                        name="desctiption" 
+                        component="textarea" 
+                        className="form-control"
+                        required
+                    />
+                </div>
 
-            </fieldset>
-            <button type="submit" className="btn btn-primary">Create</button>
+                </fieldset>
+                <button type="submit" className="btn btn-primary">Create</button>
             </form>
         )
     }
@@ -79,6 +80,7 @@ class VolunteerAdmin extends Component{
 
 VolunteerAdmin = reduxForm({
     form: 'createVolunteerResource',
+
 }, null, actions)(VolunteerAdmin);
 
 export default connect(null, actions)(VolunteerAdmin);
