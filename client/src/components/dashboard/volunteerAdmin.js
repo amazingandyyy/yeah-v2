8e367c9ea618c9a105562377adf1a58d0d590109
Dios <David@Daweis-MacBook-Pro.local>
2017-01-18 11:28:53 -0800

    React-Widgets Bug Fixed&& Add Tag-Select For VolunteerAdmin
    Fix React-Widgets Bug by:
    Reinstall url-loader,
    Rewriting the loader configuration in webpack;
    
    Add Tags for Volunteer Activities;
    
    Minor typo fix: “Description”;

--- client/src/components/dashboard/volunteerAdmin.js
+++ client/src/components/dashboard/volunteerAdmin.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import 'react-widgets/lib/less/react-widgets.less';
-import DropdownList from 'react-widgets/lib/DropdownList';
+import Multiselect from 'react-widgets/lib/Multiselect';
 
 
 import { reduxForm, Field } from 'redux-form';
@@ -12,7 +12,19 @@ class VolunteerAdmin extends Component{
         console.log('data: ', data);
         this.props.createVolunteerResource(data);
     }
+
+    renderMultiselect ({input, ...rest}) {
+
+        return (
+            <Multiselect {...input}
+                onBlur={()=> input.onBlur()}
+                value={input.value || []}
+            {...rest}/>
+        );
+    }
+
     render(){
+        const tagList = ['Major','Library','IT'];
         return (
             <form
                 onSubmit={this.props.handleSubmit(this.handleFormSubmit.bind(this))}
@@ -62,19 +74,29 @@ class VolunteerAdmin extends Component{
                 </div>
 
                 <div>
-                    <lable>Desctiption: </lable>
+                    <lable>Tags:</lable>
+                    <Field 
+                        name="tag"
+                        component={this.renderMultiselect.bind(this)}
+                        data={['Animals','Computers','Children','Environment','Education','Homeless','sports']}
+                    />
+                </div>
+
+                <div>
+                    <lable>Description: </lable>
                     <Field 
                         type="text" 
-                        name="desctiption" 
+                        name="description" 
                         component="textarea" 
                         className="form-control"
                         required
                     />
                 </div>
-
                 </fieldset>
                 <button type="submit" className="btn btn-primary">Create</button>
+            
             </form>
+            
         )
     }
 }
