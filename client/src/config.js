1aa9ffff305bb21a7d39d1cb77d851b8a82f26ca
Dios <imdavidf@gmail.com>
2017-02-11 18:01:53 -0800

    Merge remote-tracking branch 'refs/remotes/origin/master' into david
    # Conflicts:
    #	client/src/config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import production_config from './config/production/config';
-=======
-import production_config from '../production/config';
->>>>>>> refs/remotes/origin/master
-
 // const NODE_ENV = 'production';
 const NODE_ENV = 'devlopment';
 let config;
 if(NODE_ENV === 'production'){
     config = {
-        base_url: production_config.base_url
+        base_url: 'http://104.131.39.34:8000'
     }
 }else{
     config = {
