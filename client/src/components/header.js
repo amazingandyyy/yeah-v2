4a4c2add01cbf085b1714b60bcf9815db44ef29b
Amazingandyyy <amazingandyyy@gmail.com>
2017-02-11 23:19:03 -0800

    finish styling header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,44 +5,64 @@ import { Link } from 'react-router';
 import $ from 'jquery';
 
 class Header extends Component {
+    renderLinks(classNames){
+        return (
+            <div className={`${classNames}`}>
+            <Link key='1' to="/">
+                <div>Application</div>
+            </Link>
+            <Link key='2' to="/">
+                <div>Annual Program</div>
+            </Link>
+            <Link key='3' to="/">
+                <div>Blog/News</div>
+            </Link>
+            <Link key='5' to="/">
+                <div>Cases</div>
+            </Link>
+            <Link key='4' to="/">
+                <div>About</div>
+            </Link>
+            <Link key='6' to="/dashboard">
+                <span className="enter-button">Dashboard</span>
+            </Link>
+            </div>
+        )
+    }
     render() {
         return (
             <div className="header-component">
             <nav>
-                <div className="container">
-                    <div className="left-nav">
-                        <Link to="/">
-                            <div className="logo">YEAH</div>
-                        </Link>
-                    </div>
-                    <div className="right-nav">
-                        <Link to="/dashboard">
-                            <div>Seasonal Program</div>
-                        </Link>
-                        <Link to="/dashboard">
-                            <div>Annual Program</div>
-                        </Link>
-                        <Link to="/dashboard">
-                            <div>Courses</div>
-                        </Link>
-                        <Link to="/dashboard">
-                            <div>Blog</div>
-                        </Link>
-                        <Link to="/dashboard">
-                            <div className="enter-button">Dashboard</div>
-                        </Link>
-                    </div>
+                <div className="nav-container">
+                <div className="left-nav">
+                    <Link to="/">
+                        <div className="logo">YEAH</div>
+                    </Link>
+                </div>
+                {this.renderLinks("right-nav hidden-md-down")}
+                <div className="right-nav hidden-lg-up">
+                    <span className="enter-button collapse-button">
+                        <i className="fa fa-bars"></i>
+                        <i className="fa fa-times"></i>
+                    </span>
+                {this.renderLinks("fullscreen-nav")}
+                </div>
                 </div>
             </nav>
             </div>
         )
     }
       componentDidMount() {
-          const $d = $(document);
-        $d.scroll(function() {
-            console.log($d.scrollTop())
-            $('nav').toggleClass('white', $d.scrollTop() >= 300);
-        });
+            const $d = $(document);
+            $d.scroll(function() {
+                console.log($d.scrollTop())
+                $('nav').toggleClass('white', $d.scrollTop() >= 100);
+            });
+
+            $('.collapse-button').on('click', ()=>{
+                $('.collapse-button').toggleClass('collapsed');
+                $('.right-nav.hidden-lg-up').toggleClass('collapsed')
+            })
       }
 }
 
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Header from '../header';
+import ReactPlayer from 'react-player'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="yeah-home">
-      <Header />
+        <Header />
         <div className="bg"></div>
       </div>
     );
