a53328dcd2a60b513bf5e8232e71089876f5d2b8
Dios <imdavidf@gmail.com>
2017-02-12 18:13:31 -0800

    Fix The Unknow 'meta' props Bug
    There was a warnging when we go to the create volunteer page. It was
    about 'meta' props.

--- client/src/components/dashboard/admin/volunteerAdmin.js
+++ client/src/components/dashboard/admin/volunteerAdmin.js
@@ -45,7 +45,7 @@ class VolunteerAdmin extends Component{
         $('.geosuggest__input.yeah-input').val('')
     }
 
-    renderMultiselect ({input, ...rest}) {
+    renderMultiselect ({input, data}) {
         return (
             <span style={{width: '100%'}}>
                 <Multiselect 
@@ -53,7 +53,7 @@ class VolunteerAdmin extends Component{
                     {...input}
                     onBlur={()=> input.onBlur()}
                     value={input.value || []}
-                    {...rest}
+                    data = {data}
                 />
             </span>
         );
