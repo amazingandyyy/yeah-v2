a6545d49b968722db717450e4f16461cf14f854c
Amazingandyyy <amazingandyyy@gmail.com>
2017-03-02 15:49:34 -0800

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,14 @@
-const config = {
-    base_url: 'https://yeah-v2-server.herokuapp.com'
+// const NODE_ENV = 'production';
+const NODE_ENV = 'devlopment';
+let config;
+if(NODE_ENV === 'production'){
+    config = {
+        base_url: 'https://yeah-v2-server.herokuapp.com'
+    }
+}else{
+    config = {
+        base_url: 'http://localhost:8000'
+    }
 }
 
 export default config;
